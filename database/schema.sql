2778baa5589d5d8ab3349d6297206fbb2db6ad5b
Dwayne Draper <deandraper82@gmail.com>
2021-07-14 12:00:23 -0500

    Pushing work to switch computers. Basic outline in progress for database

---
+++ database/schema.sql
@@ -0,0 +1,28 @@
+-- schema for Postrgres database
+-- one table for users
+CREATE TABLE IF NOT EXISTS users (
+  id serial PRIMARY KEY,
+  username varchar(255) NOT NULL,
+  -- Will I need a password field when using oauth?
+  -- password varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  first_name varchar(255) NOT NULL,
+  last_name varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp NOT NULL DEFAULT now()
+);
+
+-- table for posts
+CREATE TABLE IF NOT EXISTS posts (
+  id serial PRIMARY KEY,
+  user_id integer NOT NULL,
+  category varchar(255) NOT NULL,
+  subcategory varchar(255) NOT NULL,
+  topic varchar(255) NOT NULL,
+  type varchar(255) NOT NULL,
+  -- how do I create a field that can be a foreign key from one of three tables?
+  -- Do I need to create three tables? null allowed?
+  -- And then only one of the three tables will have a value?
+  created_at timestamp NOT NULL DEFAULT now(),
+  updated_at timestamp NOT NULL DEFAULT now()
+);
